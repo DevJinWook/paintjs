65bb570779787acb9832b7893f5e1340bd528cc7
Dev-JinUk <jwook0318@gmail.com>
2021-11-18 21:07:29 +0900

    change color

--- app.js
+++ app.js
@@ -1,4 +1,13 @@
 const canvas = document.getElementById("jsCanvas");
+const ctx = canvas.getContext("2d");
+const colors = document.getElementsByClassName("jsColor")
+
+canvas.width = 700;
+canvas.height = 700;
+
+// context Default
+ctx.strokeStyle = "#2c2c2c";
+ctx.lineWidth = "2.5";
 
 let painting = false;
 
@@ -6,22 +15,36 @@ function stopPainting() {
     painting = false;
 }
 
+function startPainting() {
+    painting = true;
+}
+
 function onMouseMove(event) {
     const x = event.offsetX;
     const y = event.offsetY;
+    if(!painting) {
+        ctx.beginPath();
+        ctx.moveTo(x, y);
+    } else {
+        ctx.lineTo(x, y);
+        ctx.stroke()
+    }
 }
 
 function onMouseDown(event) {
     painting = true;
 }
 
-function onMouseUp(event) {
-    stopPainting();
+function handleColorClick(event) {
+    const color = event.target.style.backgroundColor;
+    ctx.strokeStyle = color;
 }
 
 if(canvas) {
     canvas.addEventListener("mousemove", onMouseMove);
-    canvas.addEventListener("mousedown", onMouseDown);
-    canvas.addEventListener("mosueup", onMouseUp);
+    canvas.addEventListener("mousedown", startPainting);
+    canvas.addEventListener("mosueup", stopPainting);
     canvas.addEventListener("mouseleave", stopPainting);
 }
+
+Array.from(colors).forEach(color => color.addEventListener("click", handleColorClick))
